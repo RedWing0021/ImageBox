5cb0b01af8652abbe6bd31f855ade6cfa316aeb5
Naitik Patel <naitikpatel.np2004@gmail.com>
2024-02-23 20:38:25 +0530

    ninth

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/ImageBox/",// code added
+  base:"",// code added
   plugins: [react()],
 })
